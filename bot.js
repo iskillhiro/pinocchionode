dc30588e99f43397e7b8d78aecb5c6eb7aaa0c3e
iskillhiro <155888748+iskillhiro@users.noreply.github.com>
2024-07-29 05:43:45 +0300

    add online, statistic  system

--- bot.js
+++ bot.js
@@ -2,18 +2,18 @@ const TelegramBot = require('node-telegram-bot-api')
 const dotenv = require('dotenv')
 dotenv.config()
 
-const bot = new TelegramBot(process.env.TOKEN, { polling: true })
+const bot = new TelegramBot(process.env.TOKEN)
 
-// const setWebhook = async () => {
-// 	const url = 'https://pinocchionode-backend.onrender.com/webhook'
-// 	try {
-// 		const res = await bot.setWebHook(url)
-// 		console.log(`Webhook set to ${url}`, res)
-// 	} catch (err) {
-// 		console.error('Error setting webhook:', err)
-// 	}
-// }
+const setWebhook = async () => {
+	const url = 'https://pinocchionode-backend.onrender.com/webhook'
+	try {
+		const res = await bot.setWebHook(url)
+		console.log(`Webhook set to ${url}`, res)
+	} catch (err) {
+		console.error('Error setting webhook:', err)
+	}
+}
 
-// setWebhook()
+setWebhook()
 
 module.exports = bot
